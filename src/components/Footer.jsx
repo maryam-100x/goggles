4ca3bf28051bfb1105e463dd4f1aeb2441d6721d
solpotfun <209683647+solpotfun@users.noreply.github.com>
2025-05-15 19:07:02 +0200

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      ~ built with trench goggles on
+      built with trench goggles on
     </footer>
   );
 }
